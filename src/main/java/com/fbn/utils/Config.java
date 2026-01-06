2af0ddd6fb3836497e0d1e5e0b383b930a9c5907
Shatadipa <shatadipa1986@gmail.com>
2026-01-07 00:09:45 +0530

    Adds Config file to store all configuration settings

---
+++ src/main/java/com/fbn/utils/Config.java
@@ -0,0 +1,16 @@
+package com.fbn.utils;
+
+/**
+ * Config file to store configuration settings.
+ *
+ * This is preferred over configuration in non code file since storing this here provides us type-safety
+ * and other static code analysis checks.
+ */
+public abstract class Config {
+    private Config(){}
+
+    public static final String BASE_URL = "http://128.251.99.120/obnadminportal";
+    public static final String LOGIN_URL = "/login";
+
+    public static final int TIMEOUT_IN_SECONDS = 30;
+}
