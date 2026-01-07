59eada661b4e9f98e5013bac7335dce4c47ffbda
Shatadipa <shatadipa1986@gmail.com>
2026-01-07 23:31:04 +0530

    Removes hard coded test data from tests to use the test data from the json file

--- src/test/java/com/fbn/LoginTest.java
+++ src/test/java/com/fbn/LoginTest.java
@@ -2,6 +2,8 @@ package com.fbn;
 
 import com.fbn.pages.HomePage;
 import com.fbn.pages.LoginPage;
+import com.fbn.testdata.models.LoginData;
+import com.fbn.testdata.utils.TestDataLoader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -9,11 +11,16 @@ public class LoginTest extends BaseTest{
     LoginPage loginPage;
     @Test
     public void verifyLoginWorksForValidCredentials() {
+        LoginData validLoginData = TestDataLoader.get(
+                "login.json",
+                "validUser",
+                LoginData.class
+        );
         loginPage = new LoginPage(driver);
-        loginPage.selectCountry("DRC");
-        loginPage.enterUserName("CEVA_MAKER");
-        loginPage.enterPin("Bank12!@");
-        loginPage.enterToken("12345678");
+        loginPage.selectCountry(validLoginData.getCountry());
+        loginPage.enterUserName(validLoginData.getUsername());
+        loginPage.enterPin(validLoginData.getPassword());
+        loginPage.enterToken(validLoginData.getToken());
         loginPage.clickLoginButton();
         HomePage homePage = new HomePage(driver);
         Assert.assertTrue(homePage.isSideNavPresent(), "Home Page is not loaded after logging in");
@@ -21,12 +28,18 @@ public class LoginTest extends BaseTest{
 
     @Test
     public void verifyLoginShowsErrorMessageWhenInvalidLogin(){
+        LoginData invalidLoginData = TestDataLoader.get(
+                "login.json",
+                "invalidUser",
+                LoginData.class
+        );
         loginPage = new LoginPage(driver);
-        loginPage.selectCountry("DRC");
-        loginPage.enterUserName("CEVA_MAKER");
-        loginPage.enterPin("Bank12!@");
-        loginPage.enterToken("12345678");
+        loginPage.selectCountry(invalidLoginData.getCountry());
+        loginPage.enterUserName(invalidLoginData.getUsername());
+        loginPage.enterPin(invalidLoginData.getPassword());
+        loginPage.enterToken(invalidLoginData.getToken());
         loginPage.clickLoginButton();
+
         // Ideally the following message would be an incorrect login message.
         // But during test automation the account was locked hence I have put in
         // this message to run and verify the automation code.
