547eb4c1b86e5ff639e7bc2403a58d874d466cbc
Shatadipa <shatadipa1986@gmail.com>
2026-01-07 23:29:47 +0530

    Adds LoginData POJO for testdata model

---
+++ src/test/java/com/fbn/testdata/models/LoginData.java
@@ -0,0 +1,24 @@
+package com.fbn.testdata.models;
+
+public class LoginData {
+    private String username;
+    private String password;
+    private String token;
+    private String country;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
